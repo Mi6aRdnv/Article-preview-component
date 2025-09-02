03006abb508f194447b3f7e223d49dbad664c8e7
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2025-09-02 12:54:08 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="attribution">
+				Challenge by&nbsp;
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by&nbsp;
+				<a href="https://github.com/Mi6aRdnv" target="_blank">
+					Mi6aRdnv
+				</a>
+				.
+			</div>
+		</>
+	);
+}
+
+export default App;
