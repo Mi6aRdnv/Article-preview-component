5d2d86ad8e208a717c8c502b1d2df7cb0b1b3ef3
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2025-09-03 19:34:53 +0200

    feat: create project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.css";
+import BlogPreview from "./components/BlogPreview";
 
 function App() {
 	return (
 		<>
-			<div className="attribution">
-				Challenge by&nbsp;
-				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-					Frontend Mentor
-				</a>
-				. Coded by&nbsp;
-				<a href="https://github.com/Mi6aRdnv" target="_blank">
-					Mi6aRdnv
-				</a>
-				.
-			</div>
+			<header className="header"></header>
+			<main className="main">
+				<BlogPreview />
+			</main>
+			<footer className="footer">
+				<div className="attribution">
+					Challenge by&nbsp;
+					<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+						Frontend Mentor
+					</a>
+					. Coded by&nbsp;
+					<a href="https://github.com/Mi6aRdnv" target="_blank">
+						Mi6aRdnv
+					</a>
+					.
+				</div>
+			</footer>
 		</>
 	);
 }
---
+++ src/components/BlogPreview.jsx
@@ -0,0 +1,8 @@
+import Tooltip from "./Tooltip";
+import "./BlogPreview.css";
+// import { useState } from "react";
+
+export default function BlogPreview() {
+	// const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+	return <></>;
+}
